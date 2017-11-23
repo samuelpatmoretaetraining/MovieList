385319b303773a8d6bbfc1db7c81cd3163b37964
Vaguehorizon <Vaguehorizon@users.noreply.github.com>
2017-11-23 10:29:42 +0000

    Preparing project for data

--- app/src/main/java/com/muelpatmore/movielist/MainActivity.java
+++ app/src/main/java/com/muelpatmore/movielist/MainActivity.java
@@ -1,13 +1,25 @@
 package com.muelpatmore.movielist;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.frameLayout, MovieListFragment.newInstance(1))
+                    .commit();
+        }
+
     }
 }
---
+++ app/src/main/java/com/muelpatmore/movielist/NetworkCalls.java
@@ -0,0 +1,8 @@
+package com.muelpatmore.movielist;
+
+/**
+ * Created by Samuel on 23/11/2017.
+ */
+
+public class NetworkCalls {
+}
